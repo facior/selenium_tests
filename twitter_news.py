1c4137b764a8f0345b56df2ba46a5ac163f9d345
facior <lukasz.kubieniec00@gmail.com>
2023-06-23 13:29:21 +0200

    Twitter news checker

--- twitter_news.py
+++ twitter_news.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
-import org.openqa.selenium.Keys
+
 
 website = 'https://twitter.com/'
 path = '/Users/facior/Desktop/TESTY NAUKA/chromedriver'
@@ -23,7 +23,7 @@ time_to_login.click()
 time.sleep(2)
 
 username = driver.find_element(By.NAME, "text")
-username.send_keys("facior_")
+username.send_keys("python_tester")
 
 time.sleep(3)
 
@@ -33,14 +33,20 @@ next.click()
 time.sleep(2)
 
 password = driver.find_element(By.NAME, "password")
-password.send_keys("testowe_haslo")
+password.send_keys("passw0rd")
 
 login = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div")
 login.click()
 
+time.sleep(1)
+
 input_news = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input")
 input_news.send_keys("news")
-WebElement.sendKeys(Keys.ENTER)
+time.sleep(1)
+input_news.send_keys(Keys.ENTER)
+
+latest_news = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div[2]/nav/div/div[2]/div/div[2]/a")
+latest_news.click()
 
 #driver.quit()
 
